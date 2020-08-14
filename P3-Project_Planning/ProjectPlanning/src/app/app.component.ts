f17540b0ec4d708e2adf47618cfd7932e79ef723
Brookelynn Constant <Brookelynn.constant@gmail.com>
2020-08-14 16:28:13 -0500

    project planning page

---
+++ P3-Project_Planning/ProjectPlanning/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Project Planning';
+  subtitle = 'Assignment 2'
+  date = 'August 14 2020'
+}
